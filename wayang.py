9793eb1c4454492465547391b45f8bc1a389d133
wendra08 <wendrasuryananda3@gmail.com>
2023-05-31 21:22:03 +0800

    update wayang.py show persentase

--- wayang.py
+++ wayang.py
@@ -1,27 +1,45 @@
 import streamlit as st
-from tensorflow.keras.preprocessing.image import load_img,img_to_array
-from io import StringIO
-import numpy as np
 import tensorflow as tf
+from tensorflow.keras.models import load_model
+import numpy as np
+import tensorflow.keras.utils as image
 
-new_model = tf.keras.models.load_model('E:\Semester 6\Berkas Bangkit\Capstone Fix\Machine_Learning\Model.h5')
+st.set_page_config(layout="wide")
 
-st.markdown("<h1 style='text-align: center;'>Predict Ala-ala</h1>", unsafe_allow_html=True)
-st.markdown("<h4 style='text-align: center;'>Abimanyu,Antasena,Bagong,Bima,Gatot Kaca</h1>", unsafe_allow_html=True)
-st.text('')
-st.text('')
+st.markdown("<h1 style='text-align: center;'>Free-Predict</h1>", unsafe_allow_html=True)
+st.markdown("<h3 style='text-align: center;'>Predict Wayang Kulit </h3>", unsafe_allow_html=True)
+st.text("")
+st.divider()
+col1, col2 = st.columns(2)
+
 class_names = ['Abimanyu', 'Antasena', 'Bagong', 'Bima', 'Gareng', 'Gatot Kaca', 'Hanoman', 'Krisna', 'Petruk', 'Semar']
 
-uploaded_files = st.file_uploader("Choose a file", type=['png', 'jpg', 'jpeg'])
-if uploaded_files is not None:
-    bytes_data = uploaded_files.getvalue()
-    st.image(bytes_data)
-    image = load_img(uploaded_files,target_size=(150,150))
-    x = img_to_array(image)
-    x = x/255.0
-    x = np.expand_dims(x, axis=0)
-    images = np.vstack([x])
+with st.spinner('Compiling & Load the model..'):
+    model = load_model('Model.h5',compile=False)
+    model.compile(optimizer = 'adam', 
+                loss = 'categorical_crossentropy', 
+                metrics = ['accuracy'])
 
-    pred = new_model.predict(images)
-    pred = class_names[np.argmax(pred)]
-    st.header(f"Predictions: {pred}")
+with col1:
+    uploaded_files = st.file_uploader("Choose a file", type=['png', 'jpg','jpeg'])
+    if uploaded_files is not None:
+        bytes_data = uploaded_files.getvalue()
+        st.image(bytes_data)
+
+        with st.spinner('Predicting...'):
+            img = image.load_img(uploaded_files, target_size=(299,299,3))
+            x = image.img_to_array(img)
+            x = x/255.0
+            x = np.expand_dims(x, axis=0)
+            images = np.vstack([x])
+
+            pred = model.predict(images)
+            
+with col2:
+    if uploaded_files is not None:
+        predict_list = np.round(pred,3)
+        for i,j in enumerate(predict_list[0]):
+            st.text(class_names[i])
+            st.progress(float(j), text=f'{str(np.round(j,2))}%')
+
+
